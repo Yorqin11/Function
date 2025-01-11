cc3a1984271850dfd39c719cad651c4fc65a9ab3
Yorqin11 <yorqinturgunboyev5@gmail.com>
2025-01-11 15:01:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('.btn1').addEventListener('click', () => {
+    document.body.style.backgroundColor = '#FFFFFF'; 
+    document.body.style.color = '#000000';
+});
+
+document.querySelector('.btn2').addEventListener('click', () => {
+    document.body.style.backgroundColor = '#000000'; 
+    document.body.style.color = '#FFFFFF'; 
+});
